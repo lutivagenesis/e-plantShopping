7c4e1fff31fd6c5f8fc2924827c421c395f239ae
lutivagenesis <138804952+lutivagenesis@users.noreply.github.com>
2026-01-15 01:53:13 +0800

    Update CartSlice.jsx

--- src/redux/CartSlice.jsx
+++ src/redux/CartSlice.jsx
@@ -1,9 +1,21 @@
 const initialState = [];
 
-const CartSlice = {
-  addItem: () => {},
-  removeItem: () => {},
-  updateQuantity: () => {},
+export const addItem = (state, item) => {
+  const existing = state.find((i) => i.id === item.id);
+  if (existing) {
+    existing.quantity += 1;
+  } else {
+    state.push({ ...item, quantity: 1 });
+  }
 };
 
-export default CartSlice;
+export const removeItem = (state, id) => {
+  return state.filter((item) => item.id !== id);
+};
+
+export const updateQuantity = (state, { id, quantity }) => {
+  const item = state.find((i) => i.id === id);
+  if (item) {
+    item.quantity = quantity;
+  }
+};
